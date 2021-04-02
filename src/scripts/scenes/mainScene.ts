6442421306e4c13c8b5a20692c3a8f93cc18baf4
ishikagovil <ishikagovil@gmail.com>
2021-04-02 12:17:53 -0400

    modifying test main screen

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,15 +2,12 @@ import PhaserLogo from '../objects/phaserLogo'
 
 export default class MainScene extends Phaser.Scene {
   private background;
-  
+
   constructor() {
     super({ key: 'MainScene' })
   }
   create() {
     new PhaserLogo(this, this.cameras.main.width / 2, 0)
   }
-  initBackground() {
-    this.background = this.add.tileSprite(0, 0, this.gameWidth, this.gameHeight, "background").setOrigin(0, 0).setScrollFactor(0);
-    this.background.tilePositionX = this.cameras.main.scrollX * .3;
-}
+  
 }
