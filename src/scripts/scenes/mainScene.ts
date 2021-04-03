aa6baff714813477cb2b168d78f687f233c00756
Aidan Conley <aidancon@udel.edu>
2021-04-02 23:32:47 -0400

    movement

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,12 +2,32 @@ import PhaserLogo from '../objects/phaserLogo'
 
 export default class MainScene extends Phaser.Scene {
   private background;
+  player:PhaserLogo;
 
   constructor() {
     super({ key: 'MainScene' })
   }
   create() {
-    new PhaserLogo(this, this.cameras.main.width / 2, 0)
+    this.player = new PhaserLogo(this, this.cameras.main.width / 2, 0);
   }
-  
+
+  update(){
+    var cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.up.isDown) {
+			this.player.y = this.player.y - 1.5;
+			this.player.angle = 90;
+		}
+		if (cursors.down.isDown) {
+			this.player.y = this.player.y + 1.5;
+			this.player.angle = 270;
+		}
+		if (cursors.left.isDown) {
+			this.player.x = this.player.x - 1.5;
+			this.player.angle = 0;
+		}
+		if (cursors.right.isDown) {
+			this.player.x = this.player.x + 1.5;
+			this.player.angle = 180;
+		}
+  }
 }
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -1,13 +1,15 @@
 export default class PreloadScene extends Phaser.Scene {
+  
   constructor() {
     super({ key: 'PreloadScene' })
   }
   preload() {
-    this.load.image('temp', 'assets/img/phaser-logo.png')
+    this.load.image('fish', 'assets/img/phaser-logo.png')
     this.load.image('phaser-logo', 'assets/Images/Pipes.png')
 		this.load.tilemapTiledJSON('sewer1', 'assets/Maps/Sewer1.json')
   }
   create() {
+    this.add.text(20, 20, "Loading game...");
     this.scene.start('MainScene')
   }
 }
