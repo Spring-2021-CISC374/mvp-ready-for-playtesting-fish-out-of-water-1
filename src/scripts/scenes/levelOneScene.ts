e58dff096a2873878acf2971f74b4dee50dc5322
ishikagovil <ishikagovil@gmail.com>
2021-04-04 12:44:44 -0400

    Debugging collisions between player and map

--- src/scripts/scenes/levelOneScene.ts
+++ src/scripts/scenes/levelOneScene.ts
@@ -1,45 +1,51 @@
-import PhaserLogo from '../objects/phaserLogo'
 
 export default class LevelOneScene extends Phaser.Scene {
-  player:PhaserLogo;
-  private camera;
+  flounder: Phaser.Physics.Arcade.Sprite
 
   constructor() {
     super({ key: 'LevelOneScene' })
   }
   create() {
-	//Create map from Tiled
+	//Create map from Tiled and add necessary collisions
 	const map = this.make.tilemap({key: "sewer1"})
-    const tileset = map.addTilesetImage('Pipes', 'pipes')
-	const background = map.createLayer('Tile Layer 1', tileset)
-	background.setCollisionFromCollisionGroup(true)
-	//Create player (temp)
-	this.player = new PhaserLogo(this, 0, 0)
-	this.player.setScale(0.15)
-	this.player.setCollideWorldBounds(true)
-	//Initialize cameras to follow player
-	//this.cameras.main.setBounds(0, 0,this.game.config.maxWidth, this.game.config.maxHeight)
-	//this.physics.world.setBounds(0, 0,512, 512)
-	this.cameras.main.startFollow(this.player)
+	const tileset = map.addTilesetImage('Pipes', 'pipes')
+	//const background = map.createLayer('Background', tileset)
+	const foreground = map.createLayer('Sewer', tileset)
+	foreground.setCollisionByProperty({collides: true})
+
+	//debug collisions
+	const debugGraphics = this.add.graphics().setAlpha(0.7);
+	foreground.renderDebug(debugGraphics, {
+		tileColor: null,
+		collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
+		faceColor: new Phaser.Display.Color(40,39,37,255)
+	})
+
+	//Create fish
+	this.flounder = this.physics.add.sprite(0,435,'flounder', 'Flounder 1.png').setScale(0.1)
+	this.flounder.anims.play('flounder-idle')
+	this.physics.add.collider(this.flounder, foreground)
+	//Initialize cameras to follow fish
+	this.cameras.main.startFollow(this.flounder,true)
   }
 
   update(){
     var cursors = this.input.keyboard.createCursorKeys();
     if (cursors.up.isDown) {
-			this.player.y = this.player.y - 1.5;
-			this.player.angle = 90;
+			this.flounder.y = this.flounder.y - 1.5;
+			this.flounder.angle = 270;
 		}
 		if (cursors.down.isDown) {
-			this.player.y = this.player.y + 1.5;
-			this.player.angle = 270;
+			this.flounder.y = this.flounder.y + 1.5;
+			this.flounder.angle = 90;
 		}
 		if (cursors.left.isDown) {
-			this.player.x = this.player.x - 1.5;
-			this.player.angle = 0;
+			this.flounder.x = this.flounder.x - 1.5;
+			this.flounder.angle = 180;
 		}
 		if (cursors.right.isDown) {
-			this.player.x = this.player.x + 1.5;
-			this.player.angle = 180;
+			this.flounder.x = this.flounder.x + 1.5;
+			this.flounder.angle = 0;
 		}
   }
 }
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -7,9 +7,18 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.tilemapTiledJSON('sewer1', 'assets/Maps/Sewer1.json')
     this.load.image('pipes', 'assets/img/Pipes.png')
     this.load.image('phaser-logo', 'assets/img/phaser-logo.png')
+    this.load.atlas('flounder', 'assets/Sprites/Flounder.png','assets/Sprites/Flounder.json' )
   }
   create() {
     this.add.text(20, 20, "Loading game...");
     this.scene.start('LevelOneScene')
+
+    this.anims.create({
+      key: 'flounder-idle',
+      frames: this.anims.generateFrameNames( 'flounder', {start: 1, end: 2, prefix: 'Flounder ', suffix: '.png'}),
+      repeat: -1,
+      frameRate: 15
+    })
+    
   }
 }
