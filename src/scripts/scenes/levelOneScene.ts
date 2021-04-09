aea56eecb82020513c98256ffa47f933084f9164
Aidan Conley <aidancon@udel.edu>
2021-04-09 17:08:55 -0400

    fix: a few minor bug fixes, refactors

--- src/scripts/scenes/levelOneScene.ts
+++ src/scripts/scenes/levelOneScene.ts
@@ -1,5 +1,5 @@
 export default class LevelOneScene extends Phaser.Scene {
-	flounder: Phaser.Physics.Arcade.Sprite
+	player: Phaser.Physics.Arcade.Sprite
 	npc: Phaser.Physics.Arcade.Sprite
 	music:Phaser.Sound.BaseSound;
 	//Map information
@@ -7,6 +7,11 @@ export default class LevelOneScene extends Phaser.Scene {
 	background:Phaser.Tilemaps.TilemapLayer;
 	colls:Phaser.Tilemaps.TilemapLayer;
 	tileset: Phaser.Tilemaps.Tileset;
+	npcTalk: Phaser.GameObjects.Text;
+	inputElement: Phaser.GameObjects.DOMElement
+  	dir_msg: Phaser.GameObjects.Text;
+  	pl_model_key: string;
+	pause: boolean;
 	
   
 	constructor() {
@@ -36,61 +41,69 @@ export default class LevelOneScene extends Phaser.Scene {
 	  })
   
 	  //Create fish + test npc
-	  this.flounder = this.physics.add.sprite(0,435,'flounder').setScale(0.08)
+	  this.player = this.physics.add.sprite(0,435,'flounder').setScale(0.08)
 	  this.npc = this.physics.add.sprite(100,435,'flounder').setScale(0.08)
-	  this.physics.world.enableBody(this.flounder)
-	  this.add.existing(this.flounder);
+	  this.physics.world.enableBody(this.player)
+	  this.add.existing(this.player);
 	  this.npc.anims.play('flounder-idle')
-	  this.flounder.anims.play('flounder-idle')
+	  this.player.anims.play('flounder-idle')
   
-	  this.physics.add.collider(this.flounder, this.colls)
-	  this.flounder.setCollideWorldBounds(true);
+	  this.physics.add.collider(this.player, this.colls)
+	  this.player.setCollideWorldBounds(true);
 	  
 	  //Initialize cameras to follow fish
-	  this.cameras.main.startFollow(this.flounder,true)
+	  this.cameras.main.startFollow(this.player,true)
 	  this.cameras.main.zoom = 3;
   
 	  //this.physics.add.collider(this.npc, foreground, this.process)
 	  this.npc.angle = 180;
-	  this.physics.add.collider(this.flounder, this.npc);
+	  this.physics.add.collider(this.player, this.npc, this.process);
+
 	  
   }
 
+  //Where the collision between npc and flounder(player) take place
+  process(flounder, npc){ 
+	//proof of collision
+	npc.setTint("0x142702");
+  }
+
   update(){
-	var velocityX = 125; // 100
+	this.npc.setVelocity(0,0);
+	var velocityX = 125; 
     var velocityY = 125
 	var prevDir = 0;
 	let framesPerDirection:number = 3;
 	var pauseMovement = false;
-	this.flounder.setVelocity(0,0);
+	this.player.setVelocity(0,0);
 	  var cursors = this.input.keyboard.createCursorKeys();
 	  if(pauseMovement){
 		return;
 	  }
 	 if (cursors.up.isDown) {
-          this.flounder.setVelocityY(-velocityY);
-		  this.flounder.angle = 270;
+          this.player.setVelocityY(-velocityY);
+		  this.player.angle = 270;
 		  prevDir = 3;
 	  }
 	  else if (cursors.down.isDown) {
-		this.flounder.setVelocityY(velocityY);
-		this.flounder.angle = 90;
+		this.player.setVelocityY(velocityY);
+		this.player.angle = 90;
 		prevDir = 0;
 	  }
 	  else if (cursors.left.isDown) {
-		this.flounder.setVelocityX(-velocityX);
-		this.flounder.angle = 180;
+		this.player.setVelocityX(-velocityX);
+		this.player.angle = 180;
 		prevDir = 1;
 	  }
 	  else if (cursors.right.isDown) {
-		this.flounder.setVelocityX(velocityX);
-		this.flounder.angle = 0;
+		this.player.setVelocityX(velocityX);
+		this.player.angle = 0;
 		prevDir = 2;
 	  }
 	  else{
-		this.flounder.setVelocity(0,0);
-		this.flounder.setFrame(1 + (prevDir * framesPerDirection));
+		this.player.setVelocity(0,0);
+		this.player.setFrame(1 + (prevDir * framesPerDirection));
 	  }
-	  this.physics.world.collide(this.flounder,this.colls)
+	  this.physics.world.collide(this.player,this.colls)
 	}
   }
