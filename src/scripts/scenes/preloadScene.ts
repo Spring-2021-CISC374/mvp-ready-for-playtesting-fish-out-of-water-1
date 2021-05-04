7965de6943573b4348aa3c0c9eb6a0d62289e502
ishikagovil <ishikagovil@gmail.com>
2021-05-04 08:37:55 -0400

    changing first scene back to levelonescene

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -61,7 +61,7 @@ export default class PreloadScene extends Phaser.Scene {
   }
   create() {
     this.add.text(20, 20, "Loading game...");
-    this.scene.start('PipeScene');
+    this.scene.start('LevelOneScene');
 
     this.anims.create({
       key: 'clown-idle',
